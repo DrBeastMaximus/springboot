ef205aa1f61908e4839785eb7dd0be0371154351
Pino.Z <anhntps10770@fpt.edu.vn>
2022-01-11 18:03:09 +0700

    Task 1.  5:32PM 1/11/2021. JPA & Security

--- SpringBoot_JPA/src/main/java/com/tmaexample/entities/User.java
+++ SpringBoot_JPA/src/main/java/com/tmaexample/entities/User.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class User implements Serializable {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
