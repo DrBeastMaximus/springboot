4d872abcfc679812481c4b8bbf066660ab448f88
Pino.Z <anhntps10770@fpt.edu.vn>
2022-01-12 17:46:03 +0700

    Task 2.  5:45PM 1/12/2021. Spring Security & ELK

--- SpringBoot_JPA/src/main/java/com/tmaexample/controller/LogonController.java
+++ SpringBoot_JPA/src/main/java/com/tmaexample/controller/LogonController.java
@@ -1,31 +1,27 @@
 package com.tmaexample.controller;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LogonController {
     @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "index";
     }
 
     @GetMapping("/admin")
     public String admin() {
-        return "admin.html";
+        return "admin";
     }
 
     @GetMapping("/403")
     public String accessDenied() {
-        return "403.html";
+        return "403";
     }
 
     @GetMapping("/login")
     public String getLogin() {
-        return "login.html";
+        return "login";
     }
 
 }
